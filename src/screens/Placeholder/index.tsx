078181aea9110d023c15858d2a5f75da21da86f2
Santiago Bayazbakian <santiago.bayazbakian@icloud.com>
2023-03-21 18:15:53 +0100

    feat: add Placeholder screen

---
+++ src/screens/Placeholder/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Placeholder = (): JSX.Element => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Screen not available</Text>
+  </View>
+);
+
+export default Placeholder;
